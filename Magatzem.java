96a9802785d5d25716170e0728004f5664744224
23dchavez-hub <23dchavez@ibadia.cat>
2026-05-20 21:57:48 +0200

    Commit 3: Refactorització de regles de negoci completada

--- Magatzem.java
+++ Magatzem.java
@@ -18,13 +18,49 @@ class Magatzem {
     }
 
     public void actualitzarEstat() {
-        // El bucle ara només crida a una funció per a cada article
         for (int i = 0; i < articles.length; i++) {
             processarArticle(articles[i]);
         }
     }
 
     private void processarArticle(Article article) {
-        // En el següent pas posarem el contingut aquí dins
+        // REGLE 1: Martell de Thor (Llegendari) - No canvia mai de cap manera
+        if (article.nom.equals("Martell de Thor (Llegendari)")) {
+            return; 
+        }
+
+        // REGLE 2: Formatge Gidurat - Millora la qualitat amb els dies (límit 50)
+        if (article.nom.equals("Formatge Gidurat")) {
+            article.diesPerVendre--; 
+            if (article.qualitat < 50) {
+                article.qualitat++; 
+            }
+            if (article.diesPerVendre < 0 && article.qualitat < 50) {
+                article.qualitat++; // Puja el doble si ha caducat
+            }
+            return; 
+        }
+
+        // REGLE 3: Entrades per al Concert - Pujen de qualitat segons els dies. Caducades valen 0
+        if (article.nom.equals("Entrades per al Concert del Trobador")) {
+            article.diesPerVendre--; 
+            if (article.diesPerVendre < 0) {
+                article.qualitat = 0; 
+            } else {
+                if (article.qualitat < 50) article.qualitat++; 
+                if (article.diesPerVendre < 10 && article.qualitat < 50) article.qualitat++; 
+                if (article.diesPerVendre < 5 && article.qualitat < 50) article.qualitat++;  
+            }
+            return; 
+        }
+
+        // REGLE 4: Articles Normals - Baixen qualitat de 1 en 1, i el doble si han caducat (mínim 0)
+        article.diesPerVendre--; 
+        if (article.qualitat > 0) {
+            article.qualitat--; 
+        }
+        if (article.diesPerVendre < 0 && article.qualitat > 0) {
+            article.qualitat--; 
+        }
     }
 }
